3a6c67dc0be1f169d14252b854b668dedd718b18
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-23 16:19:10 -0700

    add ctor to dbcontext to allow for migration

--- src/Reciprocity/Entities/ReciprocityDbContext.cs
+++ src/Reciprocity/Entities/ReciprocityDbContext.cs
@@ -4,6 +4,11 @@ namespace Reciprocity.Entities
 {
     public class ReciprocityDbContext : DbContext
     {
+        public ReciprocityDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
         public DbSet<Category> Categories { get; set; }
     }
 }
