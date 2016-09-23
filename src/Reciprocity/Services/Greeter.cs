c0d6e14f54db06e54305bb09fee67fdd67b58479
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-23 13:49:35 -0700

    add a greeter test service and interface

---
+++ src/Reciprocity/Services/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reciprocity.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+    public class Greeter : IGreeter
+    {
+        public string GetGreeting()
+        {
+            return "The Greeter Class Greets You";
+        }
+    }
+}
