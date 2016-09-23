bc50859d9c97cbf610d293047059cf63fac525cb
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-23 15:52:25 -0700

    add initial dbcontext with category set

---
+++ src/Reciprocity/Entities/ReciprocityDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Reciprocity.Entities
+{
+    public class ReciprocityDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
