1a2f5fedcdefcb80f50c543b7da4f59f4c85a462
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-30 09:48:44 -0700

    add event and foe data services

---
+++ src/Encounter/Services/EventData.cs
@@ -0,0 +1,40 @@
+﻿using Encounter.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter.Services
+{
+    public interface IEventData
+    {
+        IEnumerable<Event> GetAll();
+        Event Get(int id);
+        void Add(Event newEvent);
+    }
+
+    public class SqlEventData : IEventData
+    {
+        private EncounterDbContext _context;
+
+        public SqlEventData(EncounterDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Event newEvent)
+        {
+            _context.Add(newEvent);
+            _context.SaveChanges();
+        }
+
+        public Event Get(int id)
+        {
+            return _context.Events.FirstOrDefault(c => c.EventId == id);
+        }
+
+        public IEnumerable<Event> GetAll()
+        {
+            return _context.Events.ToList();
+        }
+    }
+}
---
+++ src/Encounter/Services/FoeData.cs
@@ -0,0 +1,40 @@
+﻿using Encounter.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter.Services
+{
+    public interface IFoeData
+    {
+        IEnumerable<Foe> GetAll();
+        Foe Get(int id);
+        void Add(Foe newFoe);
+    }
+
+    public class SqlFoeData : IFoeData
+    {
+        private EncounterDbContext _context;
+
+        public SqlFoeData(EncounterDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Foe newFoe)
+        {
+            _context.Add(newFoe);
+            _context.SaveChanges();
+        }
+
+        public Foe Get(int id)
+        {
+            return _context.Foes.FirstOrDefault(c => c.FoeId == id);
+        }
+
+        public IEnumerable<Foe> GetAll()
+        {
+            return _context.Foes.ToList();
+        }
+    }
+}
