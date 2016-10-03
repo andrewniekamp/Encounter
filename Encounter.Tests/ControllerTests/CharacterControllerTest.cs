0f700e58a838f0ba1906ab7d8b9759b89f799e5e
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-10-03 09:03:37 -0700

    add test for controller to be adjusted for parameters

--- Encounter.Tests/ControllerTests/CharacterControllerTest.cs
+++ Encounter.Tests/ControllerTests/CharacterControllerTest.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Encounter.Controllers;
+using Encounter.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Encounter.Tests.ControllerTests
 {
-    public class CharacterControllerTest
+    [Fact]
+    public void Get_ViewResult_Index_Test()
     {
+        //arrange
+        CharacterController controller = new CharacterController();
+
+        //act
+        var result = controller.Index();
+
+        //assert
+        Assert.IsType<ViewResult>(result);
     }
 }
