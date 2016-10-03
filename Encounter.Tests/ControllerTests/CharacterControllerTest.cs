667a292aaa24595f0a4f852bb2c86ba0bf13f303
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-10-03 15:00:51 -0700

    remove some unnecessary usings

--- Encounter.Tests/ControllerTests/CharacterControllerTest.cs
+++ Encounter.Tests/ControllerTests/CharacterControllerTest.cs
@@ -1,10 +1,5 @@
 ﻿using Encounter.Controllers;
-using Encounter.Services;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
