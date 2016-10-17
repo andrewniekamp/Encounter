0966da39e8a5b8be4d607a885d7743a1ce01ca2f
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-10-17 16:34:41 -0700

    correct migrations and make ready for redeployment to azure

---
+++ src/Encounter/Migrations/20161017225552_RmOldSets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Encounter.Migrations
+{
+    public partial class RmOldSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
